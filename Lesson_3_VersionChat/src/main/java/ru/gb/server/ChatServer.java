08bc04911d4d1668ef8476ede6f4cd29b37f525a
Alexander <alexanderlukiyanovspb@gmail.com>
2021-12-02 23:20:29 +0300

    домашняя работа 3

---
+++ Lesson_3_VersionChat/src/main/java/ru/gb/server/ChatServer.java
@@ -0,0 +1,94 @@
+package ru.gb.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChatServer {
+
+    private final AuthService authService;
+    private final Map<String, ClientHandler> clients;
+
+    public ChatServer() {
+        this.authService = new SimpleAuthService();
+        this.clients = new HashMap<>();
+    }
+
+    public void run() {
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            while (true) {
+                System.out.println("Wait client connection...");
+                final Socket socket = serverSocket.accept();
+
+                JDBCAuthorization jdbcAuthorization = new JDBCAuthorization();
+                try {
+                    jdbcAuthorization.connect();
+                    jdbcAuthorization.createTable();
+                    jdbcAuthorization.insert("alexander", "123alex123");
+                    jdbcAuthorization.insert("frank", "123frank123");
+                    jdbcAuthorization.insert("ivanov", "123ivanov123");
+                    jdbcAuthorization.insert("petrov", "123petrov123");
+                    System.out.println(jdbcAuthorization.chekAccount("alexander", "123alex123"));
+                    jdbcAuthorization.dropTable();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } finally {
+                    jdbcAuthorization.disconnect();
+                }
+
+                new ClientHandler(socket, this);
+                System.out.println("Client connected");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+    public AuthService getAuthService() {
+        return authService;
+    }
+
+    public boolean isNickBusy(String nick) {
+        return clients.containsKey(nick);
+    }
+
+    public void subscribe(ClientHandler client) {
+        clients.put(client.getNick(), client);
+        broadcastClientsList();
+    }
+
+    public void unsubscribe(ClientHandler client) {
+        clients.remove(client.getNick());
+        broadcastClientsList();
+    }
+
+    public void sendMessageToClient(ClientHandler from, String nickTo, String msg) {
+        final ClientHandler client = clients.get(nickTo);
+        if (client != null) {
+            client.sendMessage("от " + from.getNick() + ": " + msg);
+            from.sendMessage("участнику " + nickTo + ": " + msg);
+            return;
+        }
+        from.sendMessage("Участника с ником " + nickTo + " нет в чат-комнате");
+    }
+
+    public void broadcastClientsList() {
+        StringBuilder clientsCommand = new StringBuilder("/clients ");
+        for (ClientHandler client : clients.values()) {
+            clientsCommand.append(client.getNick()).append(" ");
+        }
+        broadcast(clientsCommand.toString());
+    }
+
+    public void broadcast(String msg) {
+        for (ClientHandler client : clients.values()) {
+            client.sendMessage(msg);
+        }
+    }
+}
